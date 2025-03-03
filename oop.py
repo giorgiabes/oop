938f735a3dbb78524c707d8a67a9c3a37999d9ee
Giorgi Abesadze <44198003+giorgiabes@users.noreply.github.com>
2025-03-03 10:53:01 +0000

    class variables

--- oop.py
+++ oop.py
@@ -2,18 +2,24 @@
 
 
 class Employee:
+    num_of_emps = 0
+    raise_amount = 1.04
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.email = first.lower() + last.lower() + "@ecorp.com"
         self.pay = pay
 
+        Employee.num_of_emps += 1
+
     def fullname(self):
         return "{} {}".format(self.first, self.last)
 
+    def applay_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
+
 
 emp_1 = Employee("Alice", "Smith", 60000)
+print(Employee.num_of_emps)
 emp_2 = Employee("Bob", "Green", 50000)
-
-print(Employee.fullname(emp_1))
-print(emp_1.fullname())
+print(Employee.num_of_emps)
